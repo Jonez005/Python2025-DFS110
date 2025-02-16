1f9b7c427b213ac3f2e4d80b0c43ef91eb63c352
Jonez005 <kevinjonesdamasceno@gmail.com>
2025-02-16 13:32:19 -0300

    Add files via upload

---
+++ aula 3/while1.py
@@ -0,0 +1,16 @@
+# estrutura de repeticção (while)
+#variavel de controle
+print(1)
+print(2)
+print(3)
+print(4) #etc
+
+
+contador = 0
+while contador < 10:
+    print(contador)
+    contador +=1 # contador = contardor + 1
+
+
+#quando a contagem começa de 0, e nós comparamos 
+#com o sinal de '<' o valor de compração é a quantidade de repetiçoes
